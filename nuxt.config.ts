84509667472b5ab6e182e5c571f74b91166fdf45
fatdarkness6 <arsamsteam70@gmail.com>
2025-10-26 13:05:09 +0330

    experimental is added to nuxt.config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -74,7 +74,7 @@ export default defineNuxtConfig({
     },
   },
   experimental: {
-    payloadExtraction: false, // 👈 disables per-page _payload.json
+    payloadExtraction: false,
   },
   nitro: {
     prerender: {
