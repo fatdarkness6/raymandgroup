455fc652f21b785916fbefbda83ccd7fc1791745
fatdarkness6 <arsamsteam70@gmail.com>
2026-05-06 12:10:29 +0330

    Card.vue component has been developed

---
+++ components/Common/Card.vue
@@ -0,0 +1,18 @@
+<template>
+  <q-card :bordered="true" :class="`${customClass} my-card`">
+    <q-img v-if="image" :src="image" />
+    <q-card-section> </q-card-section>
+    <slot></slot>
+  </q-card>
+</template>
+<script setup lang="ts">
+const props = defineProps<{
+  customClass?: string;
+  data: any;
+  position?: "center" | "right" | "left";
+  image?: string;
+}>();
+</script>
+<style scoped>
+@import "@/assets/css/components/card.css";
+</style>
