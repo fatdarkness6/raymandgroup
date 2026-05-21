8663cc890e03e9af1094ea75118055dd64c0b028
fatdarkness6 <arsamsteam70@gmail.com>
2026-05-21 15:02:39 +0330

    q-separate from news component has been deleted

--- components/Common/News.vue
+++ components/Common/News.vue
@@ -33,7 +33,6 @@
               </div>
             </div>
           </q-card-section>
-          <q-separator />
         </CommonCard>
       </AnimationSlideOnce>
     </AnimationSlideOnceGroup>
